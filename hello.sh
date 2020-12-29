37f84b1158b47e8ab230454bd5c062b7e8627e7a
Frank Yano <frankyyano@gmail.com>
2020-12-29 16:49:03 +0300

    Redirects

--- hello.sh
+++ hello.sh
@@ -4,3 +4,7 @@
 #which bash
 
 echo "Hello World"
+
+#One of the crucial aspects of files in linux is redirect.
+#This is often achieved by employing the > operator. This however will replace the initial text.
+#When we use >> howver, this now appends the aditional text.
