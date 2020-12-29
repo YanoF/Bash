7b478221e38272698a1f0d4421eba44f7e032bb7
Frank Yano <frankyyano@gmail.com>
2020-12-29 17:01:28 +0300

    Multi-line comments.

--- hello.sh
+++ hello.sh
@@ -8,3 +8,10 @@ echo "Hello World"
 #One of the crucial aspects of files in linux is redirect.
 #This is often achieved by employing the > operator. This however will replace the initial text.
 #When we use >> howver, this now appends the aditional text.
+
+#one line comments are added using the # sign.
+# Below is how we do multi-line commenting using : ' '
+
+: '
+this is a fun way to learn bash scripting.
+I think one should learn by doing!'
