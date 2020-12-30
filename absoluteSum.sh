e31653e26ad4becbbe8a2f07e4b8528a550028ee
Frank Yano <frankyyano@gmail.com>
2020-12-30 23:56:13 +0300

    Absolute sum.

--- absoluteSum.sh
+++ absoluteSum.sh
@@ -1,5 +1,17 @@
 #! /bin/bash
 
+: '
+Write a bash script that computes the sum of the absolute ints given by standard input.
+
+The numbers are given in a single line, separated by a space. You should print the sum of their absolute
+values to standard output.
+
+For example, given the input:
+
+2 42 -15
+
+the script should print the standard output: 58 as the absolute values of the numbers in the input are
+2,41 and 15, and their sum is 58.'
 
 sum=0
 for i; do
